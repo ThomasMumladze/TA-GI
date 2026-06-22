5776ddd683ad346b5810a8186f40071a2f2aa476
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-06-22 23:28:51 +0400

    dev

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -7,12 +7,14 @@ import Home from "./pages/Home";
 import Footer from "./layout/Footer";
 
 import { useTranslation } from "./hooks/useTranslation";
+import Header from "./layout/Header";
 
 function App() {
     const { changeLanguage, t } = useTranslation();
 
     return (
         <>
+            <Header />
             <Home changeLanguage={changeLanguage} t={t} />
             <Footer changeLanguage={changeLanguage} t={t} />
         </>
