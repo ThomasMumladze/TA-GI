464ddafa83318f746f0d323989b14e6c577022d1
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-08 19:25:25 +0400

    added migration

---
+++ backEnd/Migrations/20260708152258_AddProjectTable.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "tagi_schema");
+
+            migrationBuilder.CreateTable(
+                name: "Packages",
+                schema: "tagi_schema",
+                columns: table => new
+                {
+                    PackageId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PackageTitle = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    PackageInfo = table.Column<List<string>>(type: "text[]", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    IsSale = table.Column<bool>(type: "boolean", nullable: false),
+                    Discount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packages", x => x.PackageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                schema: "tagi_schema",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Img = table.Column<string>(type: "text", maxLength: 2147483647, nullable: false),
+                    Video = table.Column<string>(type: "text", maxLength: 2147483647, nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "text", maxLength: 2147483647, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServicePackages",
+                schema: "tagi_schema",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    AboutService = table.Column<List<string>>(type: "text[]", maxLength: 2147483647, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Discount = table.Column<bool>(type: "boolean", nullable: false),
+                    DiscountPercentage = table.Column<int>(type: "integer", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServicePackages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Packages",
+                schema: "tagi_schema");
+
+            migrationBuilder.DropTable(
+                name: "Projects",
+                schema: "tagi_schema");
+
+            migrationBuilder.DropTable(
+                name: "ServicePackages",
+                schema: "tagi_schema");
+        }
+    }
+}
