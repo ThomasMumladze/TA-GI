ed3383c6bee68b903f3874ca3a7d9f2ad6a5b441
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-08 19:52:50 +0400

    updated migration

---
+++ backEnd/Migrations/20260708155156_UpdateModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServicePackages",
+                schema: "tagi_schema");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                schema: "tagi_schema",
+                table: "Projects",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                schema: "tagi_schema",
+                table: "Projects",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "tagi_schema",
+                table: "Projects",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                schema: "tagi_schema",
+                table: "Packages",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "tagi_schema",
+                table: "Packages",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                schema: "tagi_schema",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                schema: "tagi_schema",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                schema: "tagi_schema",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                schema: "tagi_schema",
+                table: "Packages");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                schema: "tagi_schema",
+                table: "Packages");
+
+            migrationBuilder.CreateTable(
+                name: "ServicePackages",
+                schema: "tagi_schema",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AboutService = table.Column<List<string>>(type: "text[]", maxLength: 2147483647, nullable: false),
+                    Discount = table.Column<bool>(type: "boolean", nullable: false),
+                    DiscountPercentage = table.Column<int>(type: "integer", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServicePackages", x => x.Id);
+                });
+        }
+    }
+}
