4a29bd0d5adc102bd29927eecf513efd0d3623fd
ThomasMumladze <98337658+ThomasMumladze@users.noreply.github.com>
2026-07-08 19:45:43 +0400

    dev

--- backEnd/Data/DataContext.cs
+++ backEnd/Data/DataContext.cs
@@ -9,7 +9,6 @@ namespace backend.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Package> Packages { get; set; }
-        public DbSet<ServicePackages> ServicePackages { get; set; }
         public DbSet<Project> Projects { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
